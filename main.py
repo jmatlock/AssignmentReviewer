a880087d97c0142a8a1d9857e3c3bbc739cafd63
jmatlock <155165+jmatlock@users.noreply.github.com>
2021-03-13 23:02:04 -0500

    Initial commit

---
+++ main.py
@@ -0,0 +1,51 @@
+# Prototype: Run target program with inputs, view outputs, visual compare, attempt auto check
+import runpy
+import sys
+import pyautogui
+import time
+import threading
+
+
+def simulate_input(file_input, stop):
+    print(f'----Input {file_input} Begin----')
+    time.sleep(1)
+    if stop():
+        print('ABORT')
+        return
+    with open(file_input, 'r') as f:
+        for line in f:
+            if stop():
+                print('ABORT')
+                return
+            if line == 'END\n':
+                break
+            print(line.strip())
+            pyautogui.typewrite(line.strip() + '\n')
+    print(f'----Input {file_input} End----')
+
+target_input_map = [('target1.py', 'input1.txt'),
+                    ('target2.py', 'input2.txt'),
+                    ('target3.py', 'input3.txt'),
+                    ('target4.py', 'input4.txt')]
+
+def main():
+    sys.stdout = open('results.txt', 'w')
+    simulated_input = None
+    for target_file, input_file in target_input_map:
+        stop = False
+        print(f'**** Target Start: {target_file}')
+        try:
+            simulated_input = threading.Thread(target=simulate_input, args=(input_file, lambda : stop, ))
+            simulated_input.start()
+            runpy.run_path(target_file)
+        except Exception as e:
+            stop = True
+            simulated_input.join()
+            print(f'Program failed:\n\t{e}')
+        finally:
+            simulated_input.join()
+            print(f'**** Target End: {target_file}\n')
+            pass
+
+if __name__ == '__main__':
+    main()
